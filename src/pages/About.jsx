69af73085191f743b579801856cadd9c84dc6819
Richard Clarke <freshnewrich@gmail.com>
2022-05-21 13:18:36 -0700

    forgot to snuggle <App/> in <Router><Router/>

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,17 @@
+import React from "react";
+import {useState,useEffect} from React
+
 export default function About(props) {
-    return <h1>About</h1>
+    //here is our state
+    const [about, setAbout] = useState(null)
+
+    //create function for api call
+    const getAboutData = async () => {
+        // make api call get response 
+        const response = await fetch(`${props.url}about`)
+        //jsonify
+        const data = await response.json()
+        //set about state to the data
+    }
+    return "a"
 }
