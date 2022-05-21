851d5a32dd97d1ad7ee54c16042dc6a7e4baaa3e
Richard Clarke <freshnewrich@gmail.com>
2022-05-21 14:12:06 -0700

    projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,3 +1,39 @@
+
+import { useState, useEffect } from "react
+
 export default function Projects(props) {
-    return <h1>Projects</h1>
+    //state
+    const [projects, setProjects] = useState(null)
+
+    //create function to make api call
+    const getProjectsData = asynce() => {
+        //api call response
+        const response = await fetch(props.URL + "projects")
+        //turn response into javascript object
+        const data = await response.json()
+        //set state
+        setProjects(data)
+    }
+
+    //initial call
+    useEffect(() => getProjectsData(), [])
+
+    //define function to return
+    const loaded = () => {
+        return projects.map((project) => (
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.image} />
+                <a href={project.git}>
+                    <button>Github</button>
+                </a>
+                <a href={project.live}>
+                    <button>live site</button>
+                </a>
+            </div>
+        ))
+
+        return projects ? loaded() : <h1>Loading...</h1>
+    }
+
 }
