2deae702b3d4ef055689f7a02bf2799a6d30f022
Richard Clarke <freshnewrich@gmail.com>
2022-05-21 13:24:05 -0700

    finish About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,6 +12,22 @@ export default function About(props) {
         //jsonify
         const data = await response.json()
         //set about state to the data
+        setAbout(data)
     }
-    return "a"
+    
+    //make initial call for data inside useEffect so it only runs at page load
+    useEffect(()=> getAboutData(), [])
+
+    //define a function that will return the JSX needed once we get the data
+    
+    const loaded = () => {
+        <div>
+            <h2>{about.name}</h2>
+            <h3>{about.email}</h3>
+            <p>{about.bio}</p>
+        </div>
+    }
+
+    //if data arrives return loaded, else loading
+    return about ? loaded() : <h1>Loading...</h1>
 }
