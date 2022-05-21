e94c6f5370325d2ad6d766b48a91edf760086177
Richard Clarke <freshnewrich@gmail.com>
2022-05-21 13:06:40 -0700

    update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,22 @@
+import {Link} from "react-router-dom"
+
 export default function Header(props) {
-    return <h2>Header here</h2>
+    const navStyle = {
+        display: "flex",
+        justifyContent: "space-around",
+        border: "3px solid black",
+        padding: "8px",
+        width: "90%",
+        margin: "auto"
+    }
+    return (
+    <header>
+        <h1>My portfolio page</h1>
+        <nav style={navStyle}>
+            <Link to="/">HOME</Link>
+            <Link to="/about">ABOUT</Link>
+            <Link to="/projects">PROJECTS</Link>
+        </nav>
+    </header>
+        )
 }
