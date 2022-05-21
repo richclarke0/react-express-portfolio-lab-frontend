35613e9775e326ca3f183e8fabd3cb5161bced07
Richard Clarke <freshnewrich@gmail.com>
2022-05-21 11:56:53 -0700

    router in main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from "./reportWebVitals"
+
+//import router
+import {BrowserRouter as Router} from "react-router-dom"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
+
+
+//reporting
+reportWebVitals()
