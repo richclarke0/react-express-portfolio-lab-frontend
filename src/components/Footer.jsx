dba2860af51fd21c0a10130e3c66b38bbd58465c
Richard Clarke <freshnewrich@gmail.com>
2022-05-25 18:45:56 -0700

    dunning kreuger effect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,3 +1,13 @@
+import styled from "styled-components"
+
+const foot = styled.div`
+background-color: #81a;
+border-radius: 8px;
+color: white;
+font-size: 14pt;
+width: 300px;
+`
+
 export default function Footer(props) {
-    return <h1>Footer</h1>
+    return <foot>Footer</foot>
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,36 @@
 import {Link} from "react-router-dom"
+import styled from "styled-components"
+
+const navStyle = styled.div`
+display: flex;
+justify-content: space-around;
+border: 3px solid black;
+padding: 8px;
+width: 90%;
+margin: auto;
+font-style: italic;
+flex-flow: row nowrap;
+`
 
 export default function Header(props) {
-    const navStyle = {
-        display: "flex",
-        justifyContent: "space-around",
-        border: "3px solid black",
-        padding: "8px",
-        width: "90%",
-        margin: "auto"
-    }
+    //this is inline css
+    // const navStyle = {
+    //     display: "flex",
+    //     justifyContent: "space-around",
+    //     border: "3px solid black",
+    //     padding: "8px",
+    //     width: "90%",
+    //     margin: "auto"
+    // }
+    // would have <nav style={navStyle}> to use it
     return (
     <header>
         <h1>My portfolio page</h1>
-        <nav style={navStyle}>
-            <Link to="/">HOME</Link>
-            <Link to="/about">ABOUT</Link>
-            <Link to="/projects">PROJECTS</Link>
-        </nav>
+        <navStyle>
+                <Link to="/">HOME</Link>
+                <Link to="/about">ABOUT</Link>
+                <Link to="/projects">PROJECTS</Link>
+        </navStyle>
     </header>
         )
 }
